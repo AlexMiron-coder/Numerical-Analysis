c142bd3e298efe5b6571b2afda1699ddb08f8e60
AlexMiron-coder <mironoww-02rus@mail.ru>
2022-03-18 15:22:48 +0300

    first commit

---
+++ lab1/lab1_1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+template <class T>
+class Matrix{
+public:
+    Matrix();
+private:
+    std::vector<std::vector<T>> _matrix;
+
+};
+
+
+int main() {
+
+    return 0;
+}
