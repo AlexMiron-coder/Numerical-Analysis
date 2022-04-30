e2ae5d5c46698d2ab3df67ceda06f6f1400fa7fc
AlexMiron-coder <mironoww-02rus@mail.ru>
2022-04-30 19:03:49 +0300

    add solver system

--- lab1/lab1_2/main.cpp
+++ lab1/lab1_2/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "tridiagonal_matrix.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    size_t n;
+    std::cin >> n;
+    TridiagonalMatrix<double> system(n);
+    std::vector<double> result = system.solve();
+    for (double i : result) {
+        std::cout << i << " ";
+    }
     return 0;
 }
+
+/*
+5
+8 4
+-5 22 8
+-5 -11 1
+-9 -15 1
+1 7
+48 125 -43 18 -23
+ */
--- lab1/lab1_2/tridiagonal_matrix.hpp
+++ lab1/lab1_2/tridiagonal_matrix.hpp
@@ -6,43 +6,79 @@
 template<class T>
 class TridiagonalMatrix{
 public:
-    TridiagonalMatrix(size_t);
+    explicit TridiagonalMatrix(size_t);
     void getSystem();
     std::vector<T> solve();
 private:
     size_t n;
-    std::vector<T> _a;
-    std::vector<T> _b;
-    std::vector<T> _c;
-    std::vector<T> _d;
+    std::vector<T> a;
+    std::vector<T> b;
+    std::vector<T> c;
+    std::vector<T> d;
 };
 
 
 // Implementation
 template<class T>
-TridiagonalMatrix<T>::TridiagonalMatrix<class T>(size_t size) : _a(size), _b(size), _c(size), _d(size) {
+TridiagonalMatrix<T>::TridiagonalMatrix(size_t size) : a(size, 0), b(size, 0), c(size, 0), d(size, 0) {
     n = size;
     getSystem();
 }
 
 template<class T>
-int TridiagonalMatrix<T>::getSystem() {
-    std::cin >> _b[0] >> _c[0]; // a[0] = 0
+void TridiagonalMatrix<T>::getSystem() {
+    std::cin >> b[0] >> c[0]; // a[0] = 0
     for (size_t i = 1; i < n - 1; i++) {
-        std::cin >> _a[i] >> _b[i] >> _c[i];
+        std::cin >> a[i] >> b[i] >> c[i];
     }
-    std::cin >> _a[n - 1] >> _b[n - 1]; // c[n - 1] = 0
+    std::cin >> a[n - 1] >> b[n - 1]; // c[n - 1] = 0
 
     for (size_t i = 0; i < n; i++) {
-        std::cin >> _d[i];
+        std::cin >> d[i];
     }
 }
 
 template<class T>
 std::vector<T> TridiagonalMatrix<T>::solve() {
-    std::vector<T> x(n, 0);
+    std::vector<T> x(n);
+    std::vector<T> p(n);
+    std::vector<T> q(n);
+    p[0] = -c[0] / b[0];
+    q[0] = d[0] / b[0];
+    for (size_t i = 1; i < n - 1; i++) {
+        p[i] = -c[i] / (b[i] + a[i] * p[i-1]);
+        q[i] = (d[i] - a[i] * q[i-1]) / (b[i] + a[i] * p[i-1]);
+    }
+    p[n - 1] = 0;
+    q[n - 1] = (d[n-1] - a[n-1]*q[n-2]) / (b[n-1] - a[n-1]*p[n-2]);
+
+    for (size_t i = 0; i < n; i++) {
+        std::cout << p[i] << " " << q[i] << "\n";
+    }
+
+    x[n - 1] = q[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+        x[i] = p[i]*x[i + 1] + q[i];
+    }
     return x;
 }
 
+/*
+5
+8 4
+-5 22 8
+-5 -11 1
+-9 -15 1
+1 7
+48 125 -43 18 -23
+
+
+4
+8 -2
+-1 6 -2
+2 10 -4
+-1 6
+6 3 8 5
+ */
 
 #endif //LAB1_2_TRIDIAGONAL_MATRIX_HPP
