5408df29c3661b77edf46b877ed8be8bf2cceefc
AlexMiron-coder <mironoww-02rus@mail.ru>
2022-04-30 16:42:37 +0300

    add matrix class

---
+++ lab1/lab1_1/cmake-build-debug/CMakeFiles/lab1_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_1.dir/main.cpp.o"
+  "lab1_1"
+  "lab1_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
